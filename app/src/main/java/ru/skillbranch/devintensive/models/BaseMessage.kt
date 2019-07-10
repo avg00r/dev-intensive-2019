e62cb78c60ea737f4e5cc16a754bb1c8b2402a6e
Vadim Yangunaev <software.tester@outlook.com>
2019-07-11 00:12:14 +0400

    task 2

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -2,17 +2,34 @@ package ru.skillbranch.devintensive.models
 
 import java.util.*
 
-abstract class BaseMessage(
+abstract class BaseMessage (
     val id: String,
     val from: User?,
     val chat: Chat,
     val isIncoming: Boolean = false,
-    val date: Date = Date() ) {
-
-    abstract fun formatMessage()
+    val date: Date = Date()
+) {
+    abstract fun formatMessage(): String
 
     companion object AbstractFactory {
+        var lastId: Int = -1
+        fun makeMessage(
+            from: User?,
+            chat: Chat,
+            date: Date = Date(),
+            type: String = "text",
+            payload: Any?,
+            isIncoming: Boolean = false
+        ): BaseMessage {
+            lastId++
+            return when (type) {
+                "image"-> ImageMessage("$lastId", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
+                "text" -> TextMessage("$lastId", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
 
-        fun makeMessage(from : User, chat : Chat, date : Date, type: String, payload : String, isIncoming: Boolean = false) {}
+                else-> if ("image" == payload || "text" == payload) makeMessage(from, chat, date, payload, type, isIncoming) else throw IllegalArgumentException()
+
+            }
+        }
+
     }
 }
